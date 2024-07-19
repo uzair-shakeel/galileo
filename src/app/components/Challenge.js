abc98ec110a3e49230c77bfe88a356359fa8c615
uzair-shakeel <uzairshakeel00000@gmail.com>
2024-07-19 18:06:11 +0500

    Challange section

--- src/app/components/Challenge.js
+++ src/app/components/Challenge.js
@@ -21,6 +21,55 @@ const Challenge = () => {
           </p>
         </div>
       </div>
+      <div className="flex gap-[3%] pt-[10%]">
+        <div className="space-y-8 w-[60%]">
+          <h2 className="text-[#5CE1E6] text-[45px] leading-[72px]">
+            The Crypto Challenge:{" "}
+            <span className="text-white">Volatility & Security</span>
+          </h2>
+          <p className="text-[18px] leading-[30px] text-white/70">
+            While the ever-changing landscape of cryptocurrency presents great
+            opportunities, the wild price swings and security risks can leave
+            your business exposed. Galileo Capital offers a safe and reliable
+            platform for managing your cryptocurrency assets.
+          </p>
+          <button className="bg-[#FCF0F8] text-black py-4 px-9 rounded-full text-[17px] font-[500]">
+            Get Started
+          </button>
+        </div>
+
+        <div className="w-[40%] space-y-8">
+          <div className="flex gap-[5%] items-center hover-gradient p-4 rounded-[20px]">
+            <img src="/assets/icon1.png" />
+            <div>
+              <h5 className="text-[20px] leading-[26px] font-[600] pb-2">
+                Regulatory Compliance
+              </h5>
+              <p>Licensed and regulated in the US and Costa Rica.</p>
+            </div>
+          </div>
+          <div className="flex gap-[5%] items-center hover-gradient p-4 rounded-[20px]">
+            <img src="/assets/icon2.png" />
+            <div>
+              <h5 className="text-[20px] leading-[26px] font-[600] pb-2">
+                Personalized Service
+              </h5>
+              <p>Dedicated account managers for tailored support.</p>
+            </div>
+          </div>
+          <div className="flex gap-[5%] items-center hover-gradient p-4 rounded-[20px]">
+            <img src="/assets/icon3.png" />
+            <div>
+              <h5 className="text-[20px] leading-[26px] font-[600] pb-2">
+                Secure Transactions
+              </h5>
+              <p>
+                Advanced blockchain technology and secure trading algorithms.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
