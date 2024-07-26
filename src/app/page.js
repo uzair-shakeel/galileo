1c231ca60ea775b1db9bccda8a5e24bf7a427302
uzair-shakeel <uzairshakeel00000@gmail.com>
2024-07-26 22:34:38 +0500

    fixed

--- src/app/page.js
+++ src/app/page.js
@@ -1,25 +1,40 @@
 "use client";
-import Image from "next/image";
-import Challenge from "./components/Features";
-import Hero from "./components/Hero";
-import Partner from "./components/Solution";
-import Security from "./components/Security";
-import Service from "./components/Service";
-import Process from "./components/Process";
-import About from "./components/About";
-import Footer from "./components/Footer";
+import dynamic from "next/dynamic";
+
+const DynamicHero = dynamic(() => import("./components/Hero"), { ssr: false });
+const DynamicChallenge = dynamic(() => import("./components/Features"), {
+  ssr: false,
+});
+const DynamicPartner = dynamic(() => import("./components/Solution"), {
+  ssr: false,
+});
+const DynamicSecurity = dynamic(() => import("./components/Security"), {
+  ssr: false,
+});
+const DynamicService = dynamic(() => import("./components/Service"), {
+  ssr: false,
+});
+const DynamicProcess = dynamic(() => import("./components/Process"), {
+  ssr: false,
+});
+const DynamicAbout = dynamic(() => import("./components/About"), {
+  ssr: false,
+});
+const DynamicFooter = dynamic(() => import("./components/Footer"), {
+  ssr: false,
+});
 
 export default function Home() {
   return (
     <div>
-      <Hero />
-      <Challenge />
-      <Partner />
-      <Security />
-      <Service />
-      <Process />
-      <About />
-      <Footer />
+      <DynamicHero />
+      <DynamicChallenge />
+      <DynamicPartner />
+      <DynamicSecurity />
+      <DynamicService />
+      <DynamicProcess />
+      <DynamicAbout />
+      <DynamicFooter />
     </div>
   );
 }
